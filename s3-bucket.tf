7567b836c839d6bd1d345a29f6d6364560bcb5a5
ramvijayan <ramanathan.vijayan@hcl.com>
2021-07-31 17:14:01 +0000

    testcode

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,6 @@
 module "s3-bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
   version = "2.6.0"
-  bucket_prefix ="RAMANATHAN"
+  bucket_prefix ="ramtest"
  # insert the 5 required variables here
 }
